5d4d01d9aaaa8e4c50265f6e051d938b194cf0b8
Sam Roeca <samuel.roeca@gmail.com>
2020-04-29 17:59:59 -0400

    Remove unnecessary executable existence checking

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## 0.2.1
+
+### Removed
+
+- Unnecessary executable checking. This ended up checking existence even when non-Python files were opened, which would be annoying for people working across multiple languages. This ended up removing a dependency (which) and relying on the better error message provided by coc out of the box.
+
 ## 0.2.0
 
 ### Added
--- src/index.ts
+++ src/index.ts
@@ -6,46 +6,24 @@ import {
   workspace,
   LanguageClient,
 } from 'coc.nvim'
-import which from 'which'
-
-export async function commandExists(command: string): Promise<boolean> {
-  return new Promise((resolve): void => {
-    which(command, (err) => resolve(err == null))
-  })
-}
 
 export async function activate(context: ExtensionContext): Promise<void> {
-  const command = 'jedi-language-server'
-
-  if (!(await commandExists(command))) {
-    workspace.showMessage(
-      'jedi-language-server executable not found, server cannot start',
-      'error'
-    )
-    return
-  }
-
   const serverOptions: ServerOptions = {
-    command,
-    args: [],
+    command: 'jedi-language-server',
   }
-
   const clientOptions: LanguageClientOptions = {
     documentSelector: ['python'],
   }
-
   const client = new LanguageClient(
     'jedi',
     'jedi-language-server',
     serverOptions,
     clientOptions
   )
-
   context.subscriptions.push(
     services.registLanguageClient(client),
-    // restart language server if relevant configuration options changed
-    // only updates based on items mounted at the 'jedi' key
     workspace.onDidChangeConfiguration(async (e) => {
+      // restart server if 'jedi.*' configuration changes
       if (e.affectsConfiguration('jedi')) {
         await client.stop()
         client.restart()
